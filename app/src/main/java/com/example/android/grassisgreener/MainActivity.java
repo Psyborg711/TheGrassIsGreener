78930be658184c63c0cc29ac5bec8e361679cd75
Psyborg711 <psyborg711@yahoo.com>
2018-03-19 21:59:46 -0500

    *performed a check for city1 can't match city2 *copied the output1 method and updated it to printout to the right fields as output2 method *added new parameter *boolean firstCity* to the method 'findOutputs' *copied the code for the kansas_city_parameters code in the findOutputs method to the omaha, chicago and minneapolis if statements

--- app/src/main/java/com/example/android/grassisgreener/MainActivity.java
+++ app/src/main/java/com/example/android/grassisgreener/MainActivity.java
@@ -44,67 +44,124 @@ public class MainActivity extends AppCompatActivity {
         // Building the parameter list
         totParams.add(param1);
 
-        // Continue if parameter2 does not match parameter1
-        if (!param2.equals(param1)) {
-            totParams.add(param2);
-            // Continue if parameter3 does not match parameter 1 or parameter2
-            if (!param3.equals(param1)) {
-                totParams.add(param3);
-                if (!param3.equals(param2))
+        // Continue if city1 does not match city2
+        if (!city1.equals(city2.toString()))
+        {
+            // Continue if parameter2 does not match parameter1
+            if (!param2.equals(param1))
+            {
+                totParams.add(param2);
+                // Continue if parameter3 does not match parameter 1 or parameter2
+                if (!param3.equals(param1))
                 {
-                    // Calling the method to output parameter values
-                    findOutputs(city1, totParams);
-                }
-                else if (param3.equals("None"))
-                {
-                    // Calling the method to output parameter values
-                    findOutputs(city1, totParams);
+                    totParams.add(param3);
+                    if (!param3.equals(param2))
+                    {
+                        // Calling the method to output parameter values
+                        findOutputs(city1, totParams,true);
+                        findOutputs(city2, totParams,false);
+                    }
+                    else if (param3.equals("None"))
+                    {
+                        // Calling the method to output parameter values
+                        findOutputs(city1, totParams,true);
+                        findOutputs(city2, totParams,false);
+                    }
+                    // Parameter has matched, making user change their choices and re-trying
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "A duplicate parameter was detected, please change and submit again.",
+                                Toast.LENGTH_SHORT).show();
+                    }
                 }
                 // Parameter has matched, making user change their choices and re-trying
-                else {
+                else
+                {
                     Toast.makeText(getApplicationContext(), "A duplicate parameter was detected, please change and submit again.",
                             Toast.LENGTH_SHORT).show();
                 }
             }
             // Parameter has matched, making user change their choices and re-trying
-            else {
+            else
+            {
                 Toast.makeText(getApplicationContext(), "A duplicate parameter was detected, please change and submit again.",
                         Toast.LENGTH_SHORT).show();
             }
         }
-        // Parameter has matched, making user change their choices and re-trying
-        else {
-            Toast.makeText(getApplicationContext(), "A duplicate parameter was detected, please change and submit again.",
+        else
+        {
+            Toast.makeText(getApplicationContext(), "First City can not match Second City, please change and submit again.",
                     Toast.LENGTH_SHORT).show();
         }
-
-
-
-
     }
 
-    private void findOutputs(String cityName, ArrayList chosenParams) {
-        int paramIndex;
+    private void findOutputs(String cityName, ArrayList chosenParams, boolean firstCity)
+    {
         String[] paramData;
 
-        if (cityName.equals("Kansas City")) {
+        if (cityName.equals("Kansas City"))
+        {
+            // set paramData to the array list
             paramData = getResources().getStringArray(R.array.kansas_city_parameters);
-
-            textOutput1(paramData, chosenParams.get(0).toString(), 1);
-            textOutput1(paramData, chosenParams.get(1).toString(), 2);
-            textOutput1(paramData, chosenParams.get(2).toString(), 3);
+            if (firstCity == true) {
+                textOutput1(paramData, chosenParams.get(0).toString(), 1);
+                textOutput1(paramData, chosenParams.get(1).toString(), 2);
+                textOutput1(paramData, chosenParams.get(2).toString(), 3);
+            }
+            else
+            {
+                textOutput2(paramData, chosenParams.get(0).toString(), 1);
+                textOutput2(paramData, chosenParams.get(1).toString(), 2);
+                textOutput2(paramData, chosenParams.get(2).toString(), 3);
+            }
         }
 
         if (cityName.equals("Omaha")) {
-            String[] myStrings = getResources().getStringArray(R.array.kansas_city_parameters);
+            // set paramData to the array list
+            paramData = getResources().getStringArray(R.array.omaha_parameters);
+            if (firstCity == true) {
+                textOutput1(paramData, chosenParams.get(0).toString(), 1);
+                textOutput1(paramData, chosenParams.get(1).toString(), 2);
+                textOutput1(paramData, chosenParams.get(2).toString(), 3);
+            }
+            else
+            {
+                textOutput2(paramData, chosenParams.get(0).toString(), 1);
+                textOutput2(paramData, chosenParams.get(1).toString(), 2);
+                textOutput2(paramData, chosenParams.get(2).toString(), 3);
+            }
         }
 
         if (cityName.equals("Chicago")) {
-            String[] myStrings = getResources().getStringArray(R.array.kansas_city_parameters);
+            // set paramData to the array list
+            paramData = getResources().getStringArray(R.array.chicago_parameters);
+            if (firstCity == true) {
+                textOutput1(paramData, chosenParams.get(0).toString(), 1);
+                textOutput1(paramData, chosenParams.get(1).toString(), 2);
+                textOutput1(paramData, chosenParams.get(2).toString(), 3);
+            }
+            else
+            {
+                textOutput2(paramData, chosenParams.get(0).toString(), 1);
+                textOutput2(paramData, chosenParams.get(1).toString(), 2);
+                textOutput2(paramData, chosenParams.get(2).toString(), 3);
+            }
         }
 
         if (cityName.equals("Minneapolis")) {
-            String[] myStrings = getResources().getStringArray(R.array.kansas_city_parameters);
+            // set paramData to the array list
+            paramData = getResources().getStringArray(R.array.minneapolis_parameters);
+            if (firstCity == true) {
+                textOutput1(paramData, chosenParams.get(0).toString(), 1);
+                textOutput1(paramData, chosenParams.get(1).toString(), 2);
+                textOutput1(paramData, chosenParams.get(2).toString(), 3);
+            }
+            else
+            {
+                textOutput2(paramData, chosenParams.get(0).toString(), 1);
+                textOutput2(paramData, chosenParams.get(1).toString(), 2);
+                textOutput2(paramData, chosenParams.get(2).toString(), 3);
+            }
         }
 
     }
@@ -196,6 +253,94 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void textOutput2(String[] cityParamData, String parameterChoice, int outputWindowNum) {
+        if (outputWindowNum == 1) {
+            // Setup Output1
+            TextView tv1 = findViewById(R.id.unit_ab);
+
+            switch (parameterChoice) {
+                case "Land Size":
+                    tv1.setText(cityParamData[0]);
+                    break;
+                case "Majority Party":
+                    tv1.setText(cityParamData[1]);
+                    break;
+                case "Population":
+                    tv1.setText(cityParamData[2]);
+                    break;
+                case "Median Household Income":
+                    tv1.setText(cityParamData[3]);
+                    break;
+                case "Median Gross Rent":
+                    tv1.setText(cityParamData[4]);
+                    break;
+                case "Percentage of Population in Poverty":
+                    tv1.setText(cityParamData[5]);
+                    break;
+                default:
+                    tv1.setText("NULL");
+                    break;
+            }
+        }
+
+        if (outputWindowNum == 2) {
+            // Setup Output2
+            TextView tv2 = findViewById(R.id.unit_bb);
+
+            switch (parameterChoice) {
+                case "Land Size":
+                    tv2.setText(cityParamData[0]);
+                    break;
+                case "Majority Party":
+                    tv2.setText(cityParamData[1]);
+                    break;
+                case "Population":
+                    tv2.setText(cityParamData[2]);
+                    break;
+                case "Median Household Income":
+                    tv2.setText(cityParamData[3]);
+                    break;
+                case "Median Gross Rent":
+                    tv2.setText(cityParamData[4]);
+                    break;
+                case "Percentage of Population in Poverty":
+                    tv2.setText(cityParamData[5]);
+                    break;
+                default:
+                    tv2.setText("NULL");
+                    break;
+            }
+        }
+        if (outputWindowNum == 3) {
+            // Setup Output3
+            TextView tv3 = findViewById(R.id.unit_cb);
+
+            switch (parameterChoice) {
+                case "Land Size":
+                    tv3.setText(cityParamData[0]);
+                    break;
+                case "Majority Party":
+                    tv3.setText(cityParamData[1]);
+                    break;
+                case "Population":
+                    tv3.setText(cityParamData[2]);
+                    break;
+                case "Median Household Income":
+                    tv3.setText(cityParamData[3]);
+                    break;
+                case "Median Gross Rent":
+                    tv3.setText(cityParamData[4]);
+                    break;
+                case "Percentage of Population in Poverty":
+                    tv3.setText(cityParamData[5]);
+                    break;
+                default:
+                    tv3.setText("NULL");
+                    break;
+            }
+        }
+    } // end of method
 }
 
 
